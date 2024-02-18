d97c28046b95dd69b77465b70635f83c85d41e60
Gabriella Muniz <gabriellamunizzz08@gmail.com>
2024-02-18 16:25:00 -0300

    alteraões para tornar a página responsiva

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import style from "../header/header.css"
@@ -5,61 +6,51 @@ import LogoPag from '/public/1.jpg'
 
 import { BsSearch } from "react-icons/bs";
 import { BsPersonCircle } from "react-icons/bs";
+import { BsX } from "react-icons/bs";
+import { BsList } from "react-icons/bs";
 
-export default function Header(){
-    return(
+
+import { useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+
+export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
         <header>
-           
-        <div className="img">
-        <Image
-               src={LogoPag}
-               alt="logo"
-               className="logo"
+            <div className="img">
+                <Image
+                    src={LogoPag}
+                    alt="logo"
+                    className="logo"
                 />
-      
-         
-         </div>
-         
-    
+            </div>
+            
+            <nav className={menuOpen ? 'show' : ''}>
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Ecobags</a></li>
+                    <li><a href="#">Promoções</a></li>
+                    <li><a href="#">Porque Investir nas Ecobags?</a></li>
+                    <li><a href="#">Sobre Nós</a></li>
+                </ul>
+            </nav>
 
- 
-     <nav>
-         <ul>
-             <li>
-                 <Link href='#'>Início</Link>
-             </li>
-             <li>
-                 <Link href='#'>Ecobags</Link>
-             </li>
-             <li>
-                 <Link href='#'>Promoções</Link>
-             </li>
-             <li>
-                 <Link href='#'>Porque Investir nas Ecobags?</Link>
-             </li>
-
-             <li>
-                 <Link href='#'>Sobre Nós</Link>
-             </li>
-         </ul>
-
-
-
-         
-     </nav>
-
-     <div className='headerIcons'>
-         <button>
-             <BsSearch />
-         </button>
-
-         <div className='loginButton'>
-             <button>
-                 <BsPersonCircle />
-                 <span>Fazer Login</span>
-             </button>
-         </div>
-</div>
-</header>
-)
+            <div className='headerIcons'>
+                <button onClick={() => setMenuOpen(!menuOpen)}>
+                    <AiOutlineMenu />
+                </button>
+                <button>
+                    <BsSearch />
+                </button>
+                <div className='loginButton'>
+                    <button>
+                        <BsPersonCircle />
+                        <span>Fazer Login</span>
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
 }
